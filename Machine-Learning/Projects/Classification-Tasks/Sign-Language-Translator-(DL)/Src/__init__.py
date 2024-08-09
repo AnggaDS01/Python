3973fc373f39932a44a0c353c3b5dc82fe28e9ef
Angga Dwi Sunarto <kanshinbatosai@gmail.com>
2024-08-09 20:11:32 +0700

    update

--- Machine-Learning/Projects/Classification-Tasks/Sign-Language-Translator-(DL)/Src/__init__.py
+++ Machine-Learning/Projects/Classification-Tasks/Sign-Language-Translator-(DL)/Src/__init__.py
@@ -1,4 +1,18 @@
 # src/__init__.py
+__version__ = '1.0.0'
+__author__ = 'Angga Dwi Sunarto'
+__all__ = [
+    'show_train_files_path_info', 
+    'show_test_files_path_info', 
+    'show_multiple_images_in_tf_dataset', 
+    'split_and_prepare_datasets', 
+    'convert_path_to_img_tf_data_train', 
+    'convert_path_to_img_tf_data_test', 
+    'one_hot_encode', 
+    'augment_image',
+    'get_nan_in_data'
+]
+
 from .show_info_tf_data import show_train_files_path_info, show_test_files_path_info
 from .display_images_tf_data import show_multiple_images_in_tf_dataset
 from .tf_data_split import split_and_prepare_datasets
