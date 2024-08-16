ebfa9aef53a6fa6fecbbb759d25cf45299649cbf
Angga Dwi Sunarto <kanshinbatosai@gmail.com>
2024-08-16 23:31:23 +0700

    update

--- Machine-Learning/Documentations/Natural-Language-Processing/Understanding-Sentiment-in-Natural-Language-with-BiLSTMs/Src/tf_data_split.py
+++ Machine-Learning/Documentations/Natural-Language-Processing/Understanding-Sentiment-in-Natural-Language-with-BiLSTMs/Src/tf_data_split.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 
 class DatasetSplitter:
-    def __init__(self, batch_size=64, train_split=0.9, shuffle_buffer_size=None, seed=None):
+    def __init__(self, batch_size=64, train_split=0.9, shuffle_buffer_size=None):
         """
         Initialize the DatasetSplitter with default or provided parameters.
 
@@ -13,7 +13,6 @@ class DatasetSplitter:
         self.batch_size = batch_size
         self.train_split = train_split
         self.shuffle_buffer_size = shuffle_buffer_size
-        self.seed = seed
 
     def split_and_prepare(self, dataset):
         """
@@ -25,40 +24,37 @@ class DatasetSplitter:
         Returns:
             tuple: A tuple containing the training and validation datasets, both batched and prefetched.
         """
-        tf.random.set_seed(self.seed)
-
         dataset_shuffled = self._shuffle_dataset(dataset)
-        train_size = int(self.train_split * len(dataset))
+        dataset_batched_and_prefetched = self._batch_and_prefetch(dataset_shuffled)
 
-        train_dataset = dataset_shuffled.take(train_size)
-        valid_dataset = dataset_shuffled.skip(train_size)
+        train_size = int(self.train_split * len(dataset_batched_and_prefetched))
+        train_dataset = dataset_batched_and_prefetched.take(train_size)
+        valid_dataset = dataset_batched_and_prefetched.skip(train_size)
 
-        train_dataset_batched = self._batch_and_prefetch(train_dataset)
-        valid_dataset_batched = self._batch_and_prefetch(valid_dataset)
+        self._display_info(dataset, train_dataset, valid_dataset)
 
-        self._display_info(train_dataset, train_dataset_batched, valid_dataset, valid_dataset_batched)
-
-        return train_dataset_batched, valid_dataset_batched
+        return train_dataset, valid_dataset
 
     def _shuffle_dataset(self, dataset):
         if self.shuffle_buffer_size is None:
             self.shuffle_buffer_size = len(dataset)
-        return dataset.shuffle(self.shuffle_buffer_size, seed=self.seed)
+        return dataset.shuffle(self.shuffle_buffer_size)
 
     def _batch_and_prefetch(self, dataset):
         return dataset.batch(self.batch_size).prefetch(tf.data.AUTOTUNE)
 
-    def _display_info(self, train_dataset, train_dataset_batched, valid_dataset, valid_dataset_batched):
+    def _display_info(self, dataset, train_dataset, valid_dataset):
+        print(f"=================================== Original Dataset ===================================")
+        print(f"Info data: {dataset}")
+        print(f"Number of data: {len(dataset)}")
+        print(f"BATCH SIZE: {self.batch_size}")
+
         print(f"=================================== Training Dataset ===================================")
         print(f"Info data: {train_dataset}")
         print(f"Training Split: {self.train_split}")
         print(f"Number of data: {len(train_dataset)}")
-        print(f"AFTER BATCH: {self.batch_size}")
-        print(f"Number of data: {len(train_dataset_batched)}")
 
         print(f"=================================== Validation Dataset ===================================")
         print(f"Info data: {valid_dataset}")
         print(f"Validation Split: {round(1 - self.train_split, 2)}")
         print(f"Number of data: {len(valid_dataset)}")
-        print(f"AFTER BATCH: {self.batch_size}")
-        print(f"Number of data: {len(valid_dataset_batched)}")
